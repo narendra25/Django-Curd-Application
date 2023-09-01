7b83b4583fc7965fbda3de0f0fc1bc4853407904
narendra25 <anrsmile96@gmail.com>
2023-09-01 14:47:11 +0530

    search bar added

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+    rollnumber=models.IntegerField()
     name=models.CharField(max_length=25,blank=False,null=False)
     email=models.EmailField()
     age=models.IntegerField()
--- app/views.py
+++ app/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render,redirect
 from .models import *
 from django.contrib import messages
+from django.core.paginator import Paginator
+from django.db.models import Q
 # Create your views here.
 
 def index(request):
-    data=Student.objects.all()
-    context={'data':data}
+    try:
+        if 'q' in request.GET:
+            q = request.GET['q']
+        # data = Data.objects.filter(last_name__icontains=q)
+        multiple_q = Q(Q(name__icontains=q) | Q(email__icontains=q))
+        data = Student.objects.filter(multiple_q) 
+        if not data:
+            messages.warning(request,"No data found")
+        else:
+            pass
+    except:
+        pass
+        data=Student.objects.all()
+    page=Paginator(data,10)
+    page_list=request.GET.get('page')
+    page=page.get_page(page_list)
+    context={'page':page}
     return render(request,'index.html',context)
 
 def about(request):
@@ -15,17 +32,18 @@ def about(request):
 def insertedData(request):
    
     if request.method=="POST":
+        RollNumber=request.POST.get('roll')
         Name=request.POST.get('name')
         Email=request.POST.get('email')
         Age=request.POST.get('age')
-        Gender=request.POST.get('gender')   
+        Gender=request.POST.get('gender')
     try:
         if Student.objects.get(email=Email):
             messages.warning(request,"Email is Already Taken")
             return redirect("/")
     except:
         pass
-    query=Student(name=Name,email=Email,age=Age,gender=Gender)
+    query=Student(rollnumber=RollNumber,name=Name,email=Email,age=Age,gender=Gender)
     messages.success(request,'Student Details Added Successfully')
     query.save()
     return redirect("/")
@@ -34,12 +52,14 @@ def insertedData(request):
 
 def update(request,id):
     if request.method=="POST":
+        RollNumber=request.POST['roll']
         Name=request.POST['name']
         Email=request.POST['email']
         Age=request.POST['age']
         Gender=request.POST['gender']
 
         edit=Student.objects.get(id=id)
+        edit.roll=RollNumber
         edit.name=Name
         edit.email=Email
         edit.age=Age
