ca4b55c4a0f23193415b02414bc5d417a763fb9a
narendra25 <anrsmile96@gmail.com>
2023-09-05 12:30:36 +0530

    album removed

--- app/models.py
+++ app/models.py
@@ -7,11 +7,7 @@ class Student(models.Model):
     email=models.EmailField()
     age=models.IntegerField()
     gender=models.CharField(max_length=25,blank=False,null=False)
-
     def __str__(self):
         return self.name
-class Album(models.Model):
-    artist = models.ForeignKey(Student,default=1, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-    num_stars = models.IntegerField()
+       
+
